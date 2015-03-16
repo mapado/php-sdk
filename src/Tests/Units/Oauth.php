f034628f793acf187f6f7e98c30cae4e40cc607d
Julien Deniau <julien.deniau@gmail.com>
2015-03-16 16:47:39 +0100

    improve tests

--- src/Tests/Units/Oauth.php
+++ src/Tests/Units/Oauth.php
@@ -4,6 +4,9 @@ namespace Mapado\Sdk\Tests\Units;
 
 use atoum;
 use GuzzleHttp\Client as HttpClient;
+use GuzzleHttp\Subscriber\Mock;
+use GuzzleHttp\Message\Response;
+use GuzzleHttp\Stream\Stream;
 use Symfony\Component\Yaml\Yaml;
 
 class Oauth extends atoum
@@ -25,22 +28,6 @@ class Oauth extends atoum
     private $clientSecret;
 
     /**
-     * username
-     *
-     * @var string
-     * @access private
-     */
-    private $username;
-
-    /**
-     * password
-     *
-     * @var string
-     * @access private
-     */
-    private $password;
-
-    /**
      * http
      *
      * @var HttpClient
@@ -57,13 +44,10 @@ class Oauth extends atoum
      */
     public function beforeTestMethod($method)
     {
-        $params = Yaml::parse(file_get_contents(__DIR__ . '/../parameters.yml'))['parameters'];
+        $this->clientId = 'client-id';
+        $this->clientSecret = 'client-secret';
 
         $this->http = new HttpClient();
-        $this->clientId = $params['client_id'];
-        $this->clientSecret = $params['client_secret'];
-        $this->username = $params['username'];
-        $this->password = $params['password'];
     }
 
     /**
@@ -74,6 +58,24 @@ class Oauth extends atoum
      */
     public function testClientToken()
     {
+        $mock = new Mock(
+            [
+                new Response(
+                    200,
+                    [],
+                    Stream::factory('{"access_token":"NzJkZTQyZTJjYjYyZjVmMDZiNzdmNDU5MjdiZTdkZTkwOWZjMTI4Y2M2ZGI2OTg3ZmQ4Y2QzMmM5ZGU3NjY3Zg","expires_in":3600,"token_type":"bearer","scope":null}')
+                ),
+                new Response(
+                    400,
+                    [],
+                    Stream::factory('{"error":"invalid_client","error_description":"The client credentials are invalid"}')
+                ),
+            ]
+        );
+
+        $this->http->getEmitter()->attach($mock);
+
+
         $this
             ->given($this->newTestedInstance($this->http, $this->clientId, $this->clientSecret))
 
@@ -81,7 +83,7 @@ class Oauth extends atoum
                 ->object($token)
                     ->isInstanceOf('\Mapado\Sdk\Model\AccessToken')
                 ->string($token->getAccessToken())
-                    ->isNotEmpty()
+                    ->isEqualTo('NzJkZTQyZTJjYjYyZjVmMDZiNzdmNDU5MjdiZTdkZTkwOWZjMTI4Y2M2ZGI2OTg3ZmQ4Y2QzMmM5ZGU3NjY3Zg')
                 ->variable($token->getRefreshToken())
                     ->isNull()
 
@@ -102,6 +104,23 @@ class Oauth extends atoum
      */
     public function testPasswordToken()
     {
+        $mock = new Mock(
+            [
+                new Response(
+                    400,
+                    [],
+                    Stream::factory('{"error":"invalid_client","error_description":"The client credentials are invalid"}')
+                ),
+                new Response(
+                    200,
+                    [],
+                    Stream::factory('{"access_token":"NTJkMTU3YzdjOThmZWNiNGY0MjYyZGZlYjYzMGJmYTcwNWE1ZDkwNWMxZjBkZmJlMTlmYmIzMjc1OTBkZDUzNg","expires_in":3600,"token_type":"bearer","scope":null,"refresh_token":"YzU4NzQ5ZWQ4NmI0ZTM1ODZjMTBjYjhkYTI3NGU2YjI5ODEzYzc5N2FjNTMxMWRlOGI5NjQ4ZWFiYzZkN2ZjMQ"}')
+                ),
+            ]
+        );
+
+        $this->http->getEmitter()->attach($mock);
+
         $this
             ->if($this->newTestedInstance($this->http, $this->clientId, $this->clientSecret))
             ->then
@@ -112,10 +131,10 @@ class Oauth extends atoum
             ->if($token = $this->testedInstance->getUserToken('test@mapado.com', 'test'))
             ->then
                 ->string($token->getAccessToken())
-                    ->isNotEmpty()
+                    ->isEqualTo('NTJkMTU3YzdjOThmZWNiNGY0MjYyZGZlYjYzMGJmYTcwNWE1ZDkwNWMxZjBkZmJlMTlmYmIzMjc1OTBkZDUzNg')
             ->then
                 ->string($token->getRefreshToken())
-                    ->isNotEmpty()
+                    ->isEqualTo('YzU4NzQ5ZWQ4NmI0ZTM1ODZjMTBjYjhkYTI3NGU2YjI5ODEzYzc5N2FjNTMxMWRlOGI5NjQ4ZWFiYzZkN2ZjMQ')
         ;
     }
 
@@ -127,16 +146,33 @@ class Oauth extends atoum
      */
     public function testRefreshToken()
     {
+        $mock = new Mock(
+            [
+                new Response(
+                    200,
+                    [],
+                    Stream::factory('{"access_token":"NTJkMTU3YzdjOThmZWNiNGY0MjYyZGZlYjYzMGJmYTcwNWE1ZDkwNWMxZjBkZmJlMTlmYmIzMjc1OTBkZDUzNg","expires_in":3600,"token_type":"bearer","scope":null,"refresh_token":"YzU4NzQ5ZWQ4NmI0ZTM1ODZjMTBjYjhkYTI3NGU2YjI5ODEzYzc5N2FjNTMxMWRlOGI5NjQ4ZWFiYzZkN2ZjMQ"}')
+                ),
+                new Response(
+                    200,
+                    [],
+                    Stream::factory('{"access_token":"MDE5OTU2MjQxMTMxMDY3Njk2NDU1ZTkyNmU3NTMwZDlmNjZlY2NiYmU1NmU2Yzk3M2FhNGMzZDFkMjVkMmY3Mw","expires_in":3600,"token_type":"bearer","scope":null,"refresh_token":"NjdhMWM2MjdmNmI3YTMzODE3ZWQ5ZjhiMWQ4YmRmOTNhYTg2MjZjZTVmZTk0NDZiYTkxZTgxYzY4MGE0YTYyYg"}')
+                ),
+            ]
+        );
+
+        $this->http->getEmitter()->attach($mock);
+
         $this
             ->given($this->newTestedInstance($this->http, $this->clientId, $this->clientSecret))
             ->and($token = $this->testedInstance->getUserToken('test@mapado.com', 'test'))
             ->if($rToken = $this->testedInstance->refreshUserToken($token->getRefreshToken()))
             ->then
                 ->string($rToken->getAccessToken())
-                    ->isNotEmpty()
                     ->isNotEqualTo($token->getAccessToken())
+                    ->isEqualTo('MDE5OTU2MjQxMTMxMDY3Njk2NDU1ZTkyNmU3NTMwZDlmNjZlY2NiYmU1NmU2Yzk3M2FhNGMzZDFkMjVkMmY3Mw')
                 ->string($rToken->getRefreshToken())
-                    ->isNotEmpty()
+                    ->isEqualTo('NjdhMWM2MjdmNmI3YTMzODE3ZWQ5ZjhiMWQ4YmRmOTNhYTg2MjZjZTVmZTk0NDZiYTkxZTgxYzY4MGE0YTYyYg')
 
         ;
     }
