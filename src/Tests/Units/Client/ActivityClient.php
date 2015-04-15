4521e377dc4f919e3aeed79581b9f7806f8f3115
Julien Deniau <julien.deniau@gmail.com>
2015-04-15 08:50:49 +0200

    add tests

--- src/Tests/Units/Client/ActivityClient.php
+++ src/Tests/Units/Client/ActivityClient.php
@@ -5,6 +5,7 @@ namespace Mapado\Sdk\Tests\Units\Client;
 use atoum;
 use GuzzleHttp\Client as HttpClient;
 use GuzzleHttp\Subscriber\Mock;
+use GuzzleHttp\Ring\Client\MockHandler;
 use GuzzleHttp\Subscriber\History;
 use GuzzleHttp\Message\Response;
 use GuzzleHttp\Stream\Stream;
@@ -55,6 +56,12 @@ class ActivityClient extends atoum
         ;
     }
 
+    /**
+     * testFindOne
+     *
+     * @access public
+     * @return void
+     */
     public function testFindOne()
     {
         $http = new HttpClient();
@@ -80,6 +87,48 @@ class ActivityClient extends atoum
             ->then
                 ->string($lastRequest->getUrl())
                 ->contains('/activities/abc?image_sizes%5B0%5D=320x160&image_sizes%5B1%5D=640x320')
+        ;
+    }
+
+    /**
+     * testProgram
+     *
+     * @access public
+     * @return void
+     */
+    public function testProgram()
+    {
+        // return the same response over and over
+        $handler = new MockHandler(
+            [
+                'status' => 200,
+                'body' => '{"data":[]}'
+            ]
+        );
+        $http = new HttpClient([ 'handler' => $handler ]);
+        $history = new History();
+        $http->getEmitter()->attach($history);
+
+        $this
+            ->given($instance = $this->newTestedInstance($http, $this->token, $this->transformer))
+            ->then
+                ->array($instance->program('uuid'))
+                ->if($lastRequest = $history->getLastRequest())
+                ->then
+                    ->string($lastRequest->getUrl())
+                    ->contains('/activities/uuid/program')
+
+            ->then
+                ->array($instance->program('uuid', ['limit' => 20]))
+                ->then
+                    ->string($history->getLastRequest()->getUrl())
+                    ->contains('/activities/uuid/program?limit=20')
+
+            ->then
+                ->array($instance->program('uuid', ['limit' => 20, 'offset' => 30]))
+                ->then
+                    ->string($history->getLastRequest()->getUrl())
+                    ->contains('/activities/uuid/program?limit=20&offset=30')
         ;
     }
 }
