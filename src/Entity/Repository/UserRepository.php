ff1c6c02ba3a8a22d09edda686269aab76b1f252
ThibaudMorand <thibaud.demorand@mapado.com>
2018-08-09 11:12:03 +0200

    add user entity

--- src/Entity/Repository/UserRepository.php
+++ src/Entity/Repository/UserRepository.php
@@ -8,31 +8,17 @@ class UserRepository extends EntityRepository
     public function me($param) {
         // generate the path to call
         // api.mapado.net/v2/me?
-        try{
-            dump($param['fields']);
-            $path = "/v2/me?fields=".$param['fields'];    
-            $data = $this->restClient->get($path);
-            return $this->sdk->getModelHydrator()->hydrate($data, $this->entityName); // hydrate for an entity, hydrateList for a list
-        }catch(\RestClientException $e) {
-            $response = $e->getResponse(); // $response should be a Psr\Http\Message\ResponseInterface
-            $body = $response->getBody();
-            dump($body); // will dump your response body
-        }
-        return;
+        dump($param['fields']);
+        $path = "/v2/me?fields=".$param['fields'];    
+        $data = $this->restClient->get($path);
+        return $this->sdk->getModelHydrator()->hydrate($data, $this->entityName); // hydrate for an entity, hydrateList for a list
     }
 
     public function getListsImported($id, $param){
         // generate the path to call
         // api.mapado.net/v2/me?
-        try{
-            $path = "/v2/users/".$id."/user-lists?fields=".$param['fields'];
-            $data = $this->restClient->get($path);        
-            return $this->sdk->getModelHydrator()->hydrateList($data, $this->entityName); // hydrate for an entity, hydrateList for a list
-        }catch(\RestClientException $e) {
-            $response = $e->getResponse(); // $response should be a Psr\Http\Message\ResponseInterface
-            $body = $response->getBody();
-            dump($body); // will dump your response body
-        }
-        return;
+        $path = "/v2/users/".$id."/user-lists?fields=".$param['fields'];
+        $data = $this->restClient->get($path);        
+        return $this->sdk->getModelHydrator()->hydrateList($data, $this->entityName); // hydrate for an entity, hydrateList for a list
     }
 }
