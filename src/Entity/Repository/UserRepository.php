e2f3ee8af00dcff5e1a2b2c0c63cabc83b64d311
ThibaudMorand <thibaud.demorand@mapado.com>
2018-08-30 16:28:20 +0200

    clean old attributes 2

--- src/Entity/Repository/UserRepository.php
+++ src/Entity/Repository/UserRepository.php
@@ -6,16 +6,12 @@ use Mapado\RestClientSdk\Exception\RestClientException;
 class UserRepository extends EntityRepository
 {
     public function me($param) {
-        // generate the path to call
-        // api.mapado.net/v2/me?
         $path = "/v2/me?fields=".$param['fields'];
         $data = $this->restClient->get($path);
         return $this->sdk->getModelHydrator()->hydrate($data, $this->entityName); // hydrate for an entity, hydrateList for a list
     }
 
     public function getListsImported($id, $param){
-        // generate the path to call
-        // api.mapado.net/v2/me?
         $path = "/v2/users/".$id."/user-lists?fields=".$param['fields'];
         $data = $this->restClient->get($path);
         return $this->sdk->getModelHydrator()->hydrateList($data, $this->entityName); // hydrate for an entity, hydrateList for a list
